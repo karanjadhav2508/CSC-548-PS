951f567164a0faf295ba64574451d670953e230f
Karan Jadhav <karanjadhav2508@gmail.com>
2017-11-24 04:33:34 -0500

    final code

--- hw6/p1/TFIDF.c
+++ hw6/p1/TFIDF.c
@@ -98,36 +98,14 @@ int main(int argc , char *argv[]){
 			if(!strcmp(file->d_name, "..")) continue;
 			numDocs++;
 		}
-		/*
-		obj obj1, obj2;
-		char *a = "karan"; strcpy(obj1.word, a);
-		char *d = "doc1"; strcpy(obj1.document, d);
-		obj1.wordCount = 10; obj1.docSize = 100;
-		obj1.numDocs = 5; obj1.numDocsWithWord = 3;
-		char *b = "shaurya"; strcpy(obj2.word, b);
-                char *d2 = "doc2"; strcpy(obj2.document, d2);
-                obj2.wordCount = 8; obj2.docSize = 90;
-                obj2.numDocs = 5; obj2.numDocsWithWord = 4;
-		TFIDF[0] = obj1;
-		TFIDF[1] = obj2;
-		*/
 	}
 
 	MPI_Bcast(&numDocs, 1, MPI_INT, 0, MPI_COMM_WORLD);
-//	MPI_Bcast(&TFIDF, 2, MPI_obj, 0, MPI_COMM_WORLD);
 	if(numDocs < numWorkers) {
 		if(rank==0)
 			printf("Number of docs lesser than number of workers.\n");
 		exit(0);
 	}
-/*
-	if(rank==1) {
-		for(int i=0; i<2; i++) {
-			obj t = TFIDF[i];
-			printf("TFIDF[%d] : %s %s %d %d %d %d\n", i, t.word, t.document, t.wordCount, t.docSize, t.numDocs, t.numDocsWithWord);
-		}
-	}
-*/
 
 	if(rank != 0) {
 		for(i = rank; i <= numDocs; i += numWorkers) {
@@ -195,7 +173,7 @@ int main(int argc , char *argv[]){
 		MPI_Send(&uw_idx, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 		MPI_Send(&unique_words, uw_idx, MPI_u_w, 0, 0, MPI_COMM_WORLD);
 		
-	} //IF rank==0
+	} //IF rank != 0
 	else {
 		int worker_TF_idx, worker_uw_idx;
 		obj worker_TFIDF[MAX_WORDS_IN_CORPUS];
@@ -215,10 +193,10 @@ int main(int argc , char *argv[]){
 			}
 			for(j=0; j<worker_uw_idx; j++) {
 				contains = 0;
-				for(int k=0; j<uw_idx; k++) {
+				for(int k=0; k<uw_idx; k++) {
                                 	if(!strcmp(unique_words[k].word, worker_unique_words[j].word)){
                                         	contains = 1;
-                                                unique_words[k].numDocsWithWord++;
+                                                unique_words[k].numDocsWithWord += worker_unique_words[j].numDocsWithWord;
                                         	break;
                                 	}
                         	}
@@ -229,121 +207,44 @@ int main(int argc , char *argv[]){
 				}				
 			}
 		}
-	}
 
-	if(rank==0) {
 		printf("-------------TF Job-------------\n");
         	for(j=0; j<TF_idx; j++)
                 	printf("%s@%s\t%d/%d\n", TFIDF[j].word, TFIDF[j].document, TFIDF[j].wordCount, TFIDF[j].docSize);
 
-		for(j=0; j<uw_idx; j++)
-			printf("%s:%d\n", unique_words[j].word, unique_words[j].numDocsWithWord);
-	}
+		for(i=0; i<TF_idx; i++) {
+                	for(j=0; j<uw_idx; j++) {
+                        	if(!strcmp(TFIDF[i].word, unique_words[j].word)) {
+                                	TFIDF[i].numDocsWithWord = unique_words[j].numDocsWithWord;
+                                	break;
+                        	}
+                	}
+        	}
 
+		printf("------------IDF Job-------------\n");
+        	for(j=0; j<TF_idx; j++)
+               		printf("%s@%s\t%d/%d\n", TFIDF[j].word, TFIDF[j].document, TFIDF[j].numDocs, TFIDF[j].numDocsWithWord);
+
+		for(j=0; j<TF_idx; j++) {
+                	double TF = 1.0 * TFIDF[j].wordCount / TFIDF[j].docSize;
+                	double IDF = log(1.0 * TFIDF[j].numDocs / TFIDF[j].numDocsWithWord);
+                	double TFIDF_value = TF * IDF;
+                	sprintf(strings[j], "%s@%s\t%.16f", TFIDF[j].document, TFIDF[j].word, TFIDF_value);
+        	}
+
+		qsort(strings, TF_idx, sizeof(char)*MAX_STRING_LENGTH, myCompare);
+        	FILE* fp = fopen("output.txt", "w");
+        	if(fp == NULL){
+                	printf("Error Opening File: output.txt\n");
+                	exit(0);
+        	}
+        	for(i=0; i<TF_idx; i++)
+                	fprintf(fp, "%s\n", strings[i]);
+        	fclose(fp);
+
+	}
 
 	MPI_Finalize();
-	/*
-	// Loop through each document and gather TFIDF variables for each word
-	for(i=1; i<=numDocs; i++){
-		sprintf(document, "doc%d", i);
-		sprintf(filename,"input/%s",document);
-		FILE* fp = fopen(filename, "r");
-		if(fp == NULL){
-			printf("Error Opening File: %s\n", filename);
-			exit(0);
-		}
-		
-		// Get the document size
-		docSize = 0;
-		while((fscanf(fp,"%s",word))!= EOF)
-			docSize++;
-		
-		// For each word in the document
-		fseek(fp, 0, SEEK_SET);
-		while((fscanf(fp,"%s",word))!= EOF){
-			contains = 0;
-			
-			// If TFIDF array already contains the word@document, just increment wordCount and break
-			for(j=0; j<TF_idx; j++) {
-				if(!strcmp(TFIDF[j].word, word) && !strcmp(TFIDF[j].document, document)){
-					contains = 1;
-					TFIDF[j].wordCount++;
-					break;
-				}
-			}
-			//If TFIDF array does not contain it, make a new one with wordCount=1
-			if(!contains) {
-				strcpy(TFIDF[TF_idx].word, word);
-				strcpy(TFIDF[TF_idx].document, document);
-				TFIDF[TF_idx].wordCount = 1;
-				TFIDF[TF_idx].docSize = docSize;
-				TFIDF[TF_idx].numDocs = numDocs;
-				TF_idx++;
-			}
-			
-			contains = 0;
-			// If unique_words array already contains the word, just increment numDocsWithWord
-			for(j=0; j<uw_idx; j++) {
-				if(!strcmp(unique_words[j].word, word)){
-					contains = 1;
-					if(unique_words[j].currDoc != i) {
-						unique_words[j].numDocsWithWord++;
-						unique_words[j].currDoc = i;
-					}
-					break;
-				}
-			}
-			
-			// If unique_words array does not contain it, make a new one with numDocsWithWord=1 
-			if(!contains) {
-				strcpy(unique_words[uw_idx].word, word);
-				unique_words[uw_idx].numDocsWithWord = 1;
-				unique_words[uw_idx].currDoc = i;
-				uw_idx++;
-			}
-		}
-		fclose(fp);
-	}
-	
-	// Print TF job similar to HW4/HW5 (For debugging purposes)
-	printf("-------------TF Job-------------\n");
-	for(j=0; j<TF_idx; j++)
-		printf("%s@%s\t%d/%d\n", TFIDF[j].word, TFIDF[j].document, TFIDF[j].wordCount, TFIDF[j].docSize);
-		
-	// Use unique_words array to populate TFIDF objects with: numDocsWithWord
-	for(i=0; i<TF_idx; i++) {
-		for(j=0; j<uw_idx; j++) {
-			if(!strcmp(TFIDF[i].word, unique_words[j].word)) {
-				TFIDF[i].numDocsWithWord = unique_words[j].numDocsWithWord;	
-				break;
-			}
-		}
-	}
-	
-	// Print IDF job similar to HW4/HW5 (For debugging purposes)
-	printf("------------IDF Job-------------\n");
-	for(j=0; j<TF_idx; j++)
-		printf("%s@%s\t%d/%d\n", TFIDF[j].word, TFIDF[j].document, TFIDF[j].numDocs, TFIDF[j].numDocsWithWord);
-		
-	// Calculates TFIDF value and puts: "document@word\tTFIDF" into strings array
-	for(j=0; j<TF_idx; j++) {
-		double TF = 1.0 * TFIDF[j].wordCount / TFIDF[j].docSize;
-		double IDF = log(1.0 * TFIDF[j].numDocs / TFIDF[j].numDocsWithWord);
-		double TFIDF_value = TF * IDF;
-		sprintf(strings[j], "%s@%s\t%.16f", TFIDF[j].document, TFIDF[j].word, TFIDF_value);
-	}
-	
-	// Sort strings and print to file
-	qsort(strings, TF_idx, sizeof(char)*MAX_STRING_LENGTH, myCompare);
-	FILE* fp = fopen("output.txt", "w");
-	if(fp == NULL){
-		printf("Error Opening File: output.txt\n");
-		exit(0);
-	}
-	for(i=0; i<TF_idx; i++)
-		fprintf(fp, "%s\n", strings[i]);
-	fclose(fp);
-	
+
 	return 0;
-	*/
 }
